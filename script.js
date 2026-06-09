8ccf97f892bc4b792400759f2303655c892941b6
Aashray Narang <aashrayofficial7@gmail.com>
2026-06-09 18:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let elem = document.querySelector("p")
+function addCount(){
+	let count = parseInt(elem.textContent)+1
+	count=count+"";
+	elem.textContent=count;
+}
